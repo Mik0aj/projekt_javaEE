117b92044f05c2416b039150e0466f5515b5563e
PawelSwitalski <49319499+pawelswitalski@users.noreply.github.com>
2021-01-17 17:18:34 +0100

    dodanie testów

--- projekt/src/main/test/java/com/database/ChatsTest.java
+++ projekt/src/main/test/java/com/database/ChatsTest.java
@@ -1,5 +1,6 @@
 package com.database;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class ChatsTest {
@@ -7,5 +8,21 @@ public class ChatsTest {
     public void deleteChatTest(){
         String chatID = "37";
         Chats.deleteChat(chatID);
+    }
+
+    @Test
+    public void getIdByEnterCodeTest(){
+        String code = "V0fbx3jChA";
+        Assertions.assertEquals("44", Chats.getIdByEnterCode(code));
+    }
+
+    @Test
+    public void getNameTest(){
+        Assertions.assertEquals("test1", Chats.getName("44"));
+    }
+
+    @Test
+    public void getIdTest(){
+        Assertions.assertEquals("44", Chats.getId("test1"));
     }
 }
